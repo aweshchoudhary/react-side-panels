2da31e0214419e37bc3a85104362e6244c36bcb5
Awesh Choudhary <aweshchoudhary7@gmail.com>
2024-05-10 14:04:59 +0530

    Added Header Comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import LeftSidePanel from "./components/left-side-panel";
 import MainContent from "./components/main-content";
+import MainHeader from "./components/main-header";
 import RightSidePanel from "./components/right-side-panel";
 
 function App() {
   return (
     <main>
-      <header className="md:h-[100px] h-[70px] flex items-center justify-center p-5 border-b bg-gray-50">
-        <h2 className="md:text-4xl text-2xl font-semibold text-center capitalize">
-          Header
-        </h2>
-      </header>
+      <MainHeader />
       <article className="flex items-start relative inset-0 w-screen h-screen">
         <LeftSidePanel />
         <MainContent />
---
+++ src/components/main-header.tsx
@@ -0,0 +1,10 @@
+const MainHeader = () => {
+  return (
+    <header className="md:h-[100px] h-[70px] flex items-center justify-center p-5 border-b bg-gray-50">
+      <h2 className="md:text-4xl text-2xl font-semibold text-center capitalize">
+        Header
+      </h2>
+    </header>
+  );
+};
+export default MainHeader;
